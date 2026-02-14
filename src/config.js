978c9fa609172f553f4e9830377424910b90edbf
HuzaifaNasirFab <huzaifanasirfab@fabtechsol.com>
2026-02-15 01:14:13 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8001/api';
+export const API_BASE_URL = 'https://huzaifanasirfab.pythonanywhere.com/api';
+// export const API_BASE_URL = 'http://localhost:8001/api';
